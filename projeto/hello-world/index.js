5f02ba4cedb668764a7278cfc628a79321e5ad15
Bernardo Belfort <153245112+bernardobelfort@users.noreply.github.com>
2026-04-07 21:08:59 -0300

    feat(hello-world): adiciona função de placar entre times

--- projeto/hello-world/index.js
+++ projeto/hello-world/index.js
@@ -8,6 +8,11 @@ function exibirArtilheiro(nome, gols) {
   return `${nome} é o artilheiro com ${gols} gols.`;
 }
 
+function exibirPlacar(time1, gols1, time2, gols2) {
+  return `${time1} ${gols1} x ${gols2} ${time2}`;
+}
+
 console.log(exibirSelecao("Brasil", "D"));
 console.log(exibirSelecao("Argentina", "A"));
 console.log(exibirArtilheiro("Vinicius Jr", 5));
+console.log(exibirPlacar("Brasil", 3, "Argentina", 1));
